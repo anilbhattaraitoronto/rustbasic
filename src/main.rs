4130c91a25abe6b844b590d32899a02030e51012
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-26 16:11:16 -0400

    Used while loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 
 fn main() {
-    loop{
-        println!("Hello again and again");
+    // let mut counter =0;
+    // let result = loop{
+    //     counter +=1;
+    //     if counter == 10 {
+    //         break counter *2;
+    //     }
+       
+    // };
+    // println!("The result is {}", result);
+    let mut number = 20;
+    while number !=0{
+        println!("Hello number {}", number);
+        number -=1;
     }
-
+    println!("While it is crazy!")
 }

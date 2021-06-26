4bc0a9b690c54aa08acf7be85d81b7b4aa6e3cf6
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-26 17:15:16 -0400

    Used several for loops for different situations

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,27 @@ fn main() {
        
     // };
     // println!("The result is {}", result);
-    let mut number = 20;
-    while number !=0{
-        println!("Hello number {}", number);
-        number -=1;
+    // let mut number = 20;
+    // while number !=0{
+    //     println!("Hello number {}", number);
+    //     number -=1;
+    // }
+    // println!("While it is crazy!")
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Other day"];
+    let len_of_days = days.len();
+    let mut index =0;
+
+    while index < len_of_days {
+        println!("They day is: {}", days[index]);
+        index += 1;
+    };
+
+    for day in days.iter(){
+        println!("Today is: {}", day);
+    };
+    for num in 1..15{
+        println!("The number is: {}", num);
     }
-    println!("While it is crazy!")
+    
 }

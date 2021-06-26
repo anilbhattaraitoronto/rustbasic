e367d1a5a4e4d3f99222fb33b1445e42235c81b9
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-26 11:05:56 -0400

    Created mutable variable and used it

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
     let first_num = 8;
     println!("The first number is {}", first_num);
+    let mut second_num = 9;
+    println!("The second number is {}", second_num);
+
+    second_num = 12;
+    println!("The reassigned second number is {}", second_num);
 }

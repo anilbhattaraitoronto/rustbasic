91ab9c7b7be3fed9ce04e8e24a51910b4e10e451
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-26 19:52:43 -0400

    Used String::from to declare string variable

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
 
+
 fn main() {
-    // let mut counter =0;
-    // let result = loop{
-    //     counter +=1;
-    //     if counter == 10 {
-    //         break counter *2;
-    //     }
-       
-    // };
-    // println!("The result is {}", result);
-    // let mut number = 20;
-    // while number !=0{
-    //     println!("Hello number {}", number);
-    //     number -=1;
-    // }
-    // println!("While it is crazy!")
-
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Other day"];
-    let len_of_days = days.len();
-    let mut index =0;
-
-    while index < len_of_days {
-        println!("They day is: {}", days[index]);
-        index += 1;
-    };
-
-    for day in days.iter(){
-        println!("Today is: {}", day);
-    };
-    for num in 1..15{
-        println!("The number is: {}", num);
-    }
+    let mut var_one = String::from("Hello");
+    // let my_text = String::from("World");
+    var_one.push_str(", World");
+    println!("The message is: {} ", var_one);
     
 }

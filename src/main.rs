f9b40dbb6ce2ecd5585eb308aa460d9e18fded30
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-26 12:38:05 -0400

    Numerical operations performed

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,17 @@ fn main() {
     println!("Speed limit is: {} ", speed);
     println!("My age is: {} ", my_age);
     println!("The earth's age is {}", earth_age);
+
+    //number operations
+
+    let total = 5 + 6;
+    let difference = 20.56 - 19.08;
+    let multiples = 5 * 6;
+    let fractions: f32 =  25.00 / 100.00;
+
+    println!("Total is {}", total);
+    println!("Difference is {} ", difference);
+    println!("Multiples are {}", multiples);
+    println!("Fractions are {}", fractions);
+
 }

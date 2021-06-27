000ac8b908516246974b35f63cb8f6bfc359e1ec
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-27 10:16:46 -0400

    used a bunch of functions to do borrowing, copying

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
 
 
 fn main() {
-    let mut var_one = String::from("Hello");
-    // let my_text = String::from("World");
-    var_one.push_str(", World");
-    println!("The message is: {} ", var_one);
-    let first_str = String::from("Ke Cha");
-    println!("The length of first string is: {}", first_str.len());
-    let second_str = first_str; //first_str is moved to second_str
-    println!("The length of second string is: {} ", second_str.len());
-    // println!("The first string is: {}", first_str);
+    let text = "hello";
+    let num: u8 = 8;
+    let mut user_text = String::from(text);
+    user_text.push_str(", world!");
+    println!("{}",user_text);
+    takes_ownership(user_text);
+    // println!("{}", user_text);
+    //this will throw compile-time error coz the value has moved
+    makes_copy(num);
+    println!("{}", num);
+    //This is fine because makes_copy copies
 
-    let third_var = String::from("This is another variable");
-    let fourth_var = third_var.clone();
+    let hello_text = gives_ownership();
 
-    println!("The third variable is: {}", third_var);
-    println!("The fourth variable is: {}", fourth_var);
-
-    //copying numerical variables
-
-    let x = 5;
-    let y = x;
-
-    println!("The value of x is {}, and that of y is {}", x, y);
+    let some_text = String::from("Hello");
+    let another_text= takes_and_gives_back(some_text);
+    println!("The hello is {} and another is {}", hello_text, another_text);
+    let length  = calculate_length(another_text).1;
+    println!("The length is {}", length);
     
+    
+}
+
+fn takes_ownership(some_string:String){
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: u8){
+    println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String{
+    let some_string = String::from("My world");
+    some_string
+}
+
+fn takes_and_gives_back(a_string:String) -> String{
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize){
+    let length = s.len();
+    (s, length)
 }

5e4870caa1f5fb231675aee55a3c451b673f3c1b
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-27 16:35:54 -0400

    Created slices branch

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 
 
 fn main() {
-    let s1 = String::from("Hello world");
-    let len = calculate_length(&s1); //reference
-    println!("The length of s1 is {}", len);
-    println!("The s1 says: {}", s1); 
-    //dereferencing is used with *
-    /* References are immutable
-        therefore, cannot be modified
-    */
+  
 }
 
-fn calculate_length(item: &String) -> usize {
-    item.len()
-    //& is reference
-}
+

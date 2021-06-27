ab13f7c583f096880a4c4797c8fe755c9ff550dc
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-26 20:08:17 -0400

    moving one variable into another -- borrowing

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,10 @@ fn main() {
     // let my_text = String::from("World");
     var_one.push_str(", World");
     println!("The message is: {} ", var_one);
+    let first_str = String::from("Ke Cha");
+    println!("The length of first string is: {}", first_str.len());
+    let second_str = first_str; //first_str is moved to second_str
+    println!("The length of second string is: {} ", second_str.len());
+    // println!("The first string is: {}", first_str);
     
 }

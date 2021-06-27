af7361df3e611edf43f7ed269f3c3931f7ee85f0
Anil Bhattarai <anilbhattarai@gmail.com>
2021-06-27 16:19:14 -0400

    Used reference

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,17 @@
 
 
 fn main() {
-    let text = "hello";
-    let num: u8 = 8;
-    let mut user_text = String::from(text);
-    user_text.push_str(", world!");
-    println!("{}",user_text);
-    takes_ownership(user_text);
-    // println!("{}", user_text);
-    //this will throw compile-time error coz the value has moved
-    makes_copy(num);
-    println!("{}", num);
-    //This is fine because makes_copy copies
-
-    let hello_text = gives_ownership();
-
-    let some_text = String::from("Hello");
-    let another_text= takes_and_gives_back(some_text);
-    println!("The hello is {} and another is {}", hello_text, another_text);
-    let length  = calculate_length(another_text).1;
-    println!("The length is {}", length);
-    
-    
+    let s1 = String::from("Hello world");
+    let len = calculate_length(&s1); //reference
+    println!("The length of s1 is {}", len);
+    println!("The s1 says: {}", s1); 
+    //dereferencing is used with *
+    /* References are immutable
+        therefore, cannot be modified
+    */
 }
 
-fn takes_ownership(some_string:String){
-    println!("{}", some_string);
-}
-
-fn makes_copy(some_integer: u8){
-    println!("{}", some_integer);
-}
-
-fn gives_ownership() -> String{
-    let some_string = String::from("My world");
-    some_string
-}
-
-fn takes_and_gives_back(a_string:String) -> String{
-    a_string
-}
-
-fn calculate_length(s: String) -> (String, usize){
-    let length = s.len();
-    (s, length)
+fn calculate_length(item: &String) -> usize {
+    item.len()
+    //& is reference
 }
